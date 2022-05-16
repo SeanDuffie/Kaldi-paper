0b71ef0e5d1db20a07cda877e8d58c39bb4bfb73
SeanDuffie <sean.duffie@tamu.edu>
2022-05-16 14:03:16 -0500

    Initial Commit

---
+++ src/main/java/me/seanduffie/latte/Latte.java
@@ -0,0 +1,17 @@
+package me.seanduffie.latte;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Latte extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
