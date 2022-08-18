f312b431b8e984257e75a6fffb74460044467ed1
SeanDuffie <sean.duffie@tamu.edu>
2022-08-17 21:12:14 -0500

    Added Recipes

--- src/main/java/me/seanduffie/kaldi/items/ItemManager.java
+++ src/main/java/me/seanduffie/kaldi/items/ItemManager.java
@@ -1,12 +1,14 @@
 package me.seanduffie.kaldi.items;
 
 import net.kyori.adventure.text.Component;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
 import org.bukkit.enchantments.Enchantment;
-import org.bukkit.inventory.ItemFlag;
-import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.*;
 import org.bukkit.inventory.meta.ItemMeta;
 
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -38,5 +40,24 @@ public class ItemManager {
         meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
         item.setItemMeta(meta);
         coffee = item;
+
+        // Shaped Recipe
+        ShapedRecipe sr = new ShapedRecipe(NamespacedKey.minecraft("coffee"), item);
+        sr.shape("C ",
+                 "B ");
+        sr.setIngredient('B', Material.BOWL);
+        sr.setIngredient('C', Material.COCOA_BEANS);
+        Bukkit.getServer().addRecipe(sr);
+
+//        // Shapeless Recipe
+//        ShapelessRecipe slr = new ShapelessRecipe(NamespacedKey.minecraft("coffee_shapeless"), item);
+//        slr.addIngredient(1, Material.BOWL);
+//        slr.addIngredient(1, Material.COCOA_BEANS);
+//        Bukkit.getServer().addRecipe(slr);
+
+        // Furnace Recipe
+        FurnaceRecipe smelt = new FurnaceRecipe(NamespacedKey.minecraft("coffee_brew"), item,
+                Material.COCOA_BEANS, 1.0f, 10 * 20);
+        Bukkit.getServer().addRecipe(smelt);
     }
 }
